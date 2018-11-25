c8fd705154a4e3e81b097b0a8f3b36f8a8fd7fba
Yusuke Hara <yh@g.ecc.u-tokyo.ac.jp>
2018-11-25 17:52:04 +0900

    標準偏差0.001で初期化

--- chainer/srcnn/model.py
+++ chainer/srcnn/model.py
@@ -7,13 +7,14 @@ class SRCNN(Chain):
         super(SRCNN, self).__init__()
         f1,f2,f3 = 9,1,5
         n1,n2 = 64,32
+        initializer = chainer.initializers.Normal(scale=0.001)
         with self.init_scope():
             self.conv1 = L.Convolution2D(
-                in_channels=3, out_channels=n1, ksize=f1, stride=1)
+                in_channels=3, out_channels=n1, ksize=f1, stride=1, initialW=initializer)
             self.conv2 = L.Convolution2D(
-                in_channels=n1, out_channels=n2, ksize=f2, stride=1)
+                in_channels=n1, out_channels=n2, ksize=f2, stride=1, initialW=initializer)
             self.conv3 = L.Convolution2D(
-                in_channels=n2, out_channels=3, ksize=f3, stride=1)
+                in_channels=n2, out_channels=3, ksize=f3, stride=1, initialW=initializer)
 
     def __call__(self, x, y):
         h = F.relu(self.conv1(x))
